5e75aa236f9071e42aba55c47a5f81b456401a2d
mikhaelHan <hancharuk270185@mail.ru>
2023-04-16 20:41:16 +0300

    feat: added binary three

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,46 +1,187 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
+  constructor () {
+    this.rootThree = null;
+  }
+
+  
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.rootThree;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+
+  add(val) {
+    const newNode = new Node(val);
+
+    if (this.rootThree === null) {
+      this.rootThree = newNode;
+      return;
+    }
+
+    let currant = this.rootThree;
+    while (currant) {
+
+      if (newNode.data < currant.data) {
+
+        if (currant.left !== null) currant = currant.left;
+        else {
+          currant.left = newNode;
+          return;
+        }
+      }
+      else {
+
+        if (currant.right !== null) currant = currant.right;
+        else {
+          currant.right = newNode;
+          return;
+        }
+      }
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+
+  has(val) {
+
+    let currant = this.rootThree;
+
+    while (currant) {
+
+      if (currant === null) return false;
+
+      if (val === currant.data) return true;
+
+      else if (val < currant.data) {
+
+        if (currant.left === null) return false;
+        else currant = currant.left;
+      }
+
+      else if (val > currant.data) {
+
+        if (currant.right === null) return false;
+        else currant = currant.right;
+      }
+    }
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+
+  find(val) {
+
+    let currant = this.rootThree;
+
+    while (currant) {
+
+      if (currant === null) return null;
+
+      if (val === currant.data) return currant;
+
+      else if (val < currant.data) {
+
+        if (currant.left === null) return null;
+        else currant = currant.left;
+      }
+
+      else if (val > currant.data) {
+
+        if (currant.right === null) return null;
+        else currant = currant.right;
+      }
+    }
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+
+  remove(val) {
+
+    const deleted = (node, val) => {
+
+      if (node === null) return null;
+
+      else if (node.data > val) {
+        node.left = deleted(node.left, val)
+        return node;
+      }
+
+      else if (node.data < val) {
+        node.right = deleted(node.right, val)
+        return node;
+      }
+
+      else if (node.data === val) {
+
+        if (node.left === null && node.right === null) return null
+
+        else if (node.left === null) return node.right;
+
+        else if (node.right === null) return node.left;
+
+        let currant = node.right;
+        while (currant.left) {
+          currant = currant.left
+        }
+
+        node.data = currant.data;
+        node.right = deleted(node.right, currant.data)
+
+        return node
+      }
+    }
+
+    this.rootThree = deleted(this.rootThree, val);
   }
+
+
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+    if (this.rootThree === null) return null;
+
+    else if (this.rootThree.left === null) return this.rootThree.data;
+
+    else {
+
+      let currant = this.rootThree;
+      while (currant) {
+
+        if (currant.left !== null) currant = currant.left;
+
+        else {
+          return currant.data;
+        }
+      }
+    }
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+    if (this.rootThree === null) return null;
+
+    else if (this.rootThree.right === null) return this.rootThree.data;
+
+    else {
+
+      let currant = this.rootThree;
+      while (currant) {
+
+        if (currant.right !== null) currant = currant.right;
+
+        else {
+          return currant.data;
+        }
+      }
+    }
   }
 }
 
